b1d62a1dc7e59295e968139b49e7e3a54b810633
Jonas <jonas@freesources.org>
2023-01-24 16:26:24 +0100

    Change wording for extra collectives action
    Signed-off-by: Jonas <jonas@freesources.org>

--- src/collectives.js
+++ src/collectives.js
@@ -3,7 +3,7 @@ import { generateUrl } from '@nextcloud/router'
 window.OCA.Collectives = {
 	...(window.OCA.Collectives || {}),
 	CollectiveExtraAction: {
-		title: t('pandoc', 'Export with pandoc'),
+		title: t('pandoc', 'Convert with pandoc'),
 		click: (fileIds) => {
 			window.open(generateUrl('/apps/pandoc?fileIds=' + fileIds.join(',')))
 		},
