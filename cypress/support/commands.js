b71b2d0629cdf819f73eb0a8059e4c720f8ce31e
Jonas <jonas@freesources.org>
2025-06-04 19:58:53 +0200

    fix(cypress): Make login work as long as we are on older cypress version
    Signed-off-by: Jonas <jonas@freesources.org>

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,10 +1,41 @@
 import axios from '@nextcloud/axios'
-import { addCommands } from '@nextcloud/cypress' // eslint-disable-line
+import { createUser, logout } from '@nextcloud/cypress/commands'
 
 const url = Cypress.config('baseUrl').replace(/\/index.php\/?$/g, '')
 Cypress.env('baseUrl', url)
 
-addCommands()
+// Copy of the new login command as long as we are blocked to upgrade @nextcloud/cypress by cypress crashes
+const login = function(user) {
+	cy.session(user, function() {
+	    cy.request('/csrftoken').then(({ body }) => {
+	        const requestToken = body.token
+	        cy.request({
+	            method: 'POST',
+	            url: '/login',
+	            body: {
+	                user: user.userId,
+	                password: user.password,
+	                requesttoken: requestToken,
+	            },
+	            headers: {
+	                'Content-Type': 'application/x-www-form-urlencoded',
+	                // Add the Origin header so that the request is not blocked by the browser.
+	                Origin: (Cypress.config('baseUrl') ?? '').replace('index.php/', ''),
+	            },
+	            followRedirect: false,
+	        })
+	    })
+	}, {
+	    validate() {
+	        cy.request('/apps/files').its('status').should('eq', 200)
+	    },
+	})
+}
+
+// Import commands from @nextcloud/cypress
+Cypress.Commands.add('login', login)
+Cypress.Commands.add('logout', logout)
+Cypress.Commands.add('createUser', createUser)
 
 Cypress.Commands.add('uploadFile', (fileName, mimeType) => {
 	return cy.fixture(fileName, 'base64')
